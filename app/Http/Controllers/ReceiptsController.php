38b5162e6e9b5aab74be8bda5f565703c0b774e9
Carlos Sirias <carlos.sirias04@gmail.com>
2023-08-27 18:54:48 -0700

    :hammer: Date Solved!

--- app/Http/Controllers/ReceiptsController.php
+++ app/Http/Controllers/ReceiptsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\DailyRecord;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class ReceiptsController extends Controller
@@ -13,12 +14,13 @@ class ReceiptsController extends Controller
     //
     public function view()
     {
+        $actualDate = Carbon::now()->format('Y-m-d');
         $sellersToday = DB::table('daily_records')
         ->select(DB::raw('DATE_FORMAT(daily_records.arrival_datetime, "%d/%m/%Y %h:%i %p") as arrival_datetime'), DB::raw('DATE_FORMAT(daily_records.departure_datetime, "%d/%m/%Y %h:%i %p") as departure_datetime'), 'seller_name', 'seller_image', 'daily_records.id')
         ->join('sellers', 'sellers.id', '=', 'daily_records.seller_id')
         ->where('agency_id', Auth::user()->id)
         ->where('active', false)
-        ->whereRaw('DATE(daily_records.arrival_datetime) = CURRENT_DATE()')
+        ->whereRaw('DATE(daily_records.arrival_datetime) = '.$actualDate)
         ->get();
 
         $result = DB::table('daily_records')
@@ -28,8 +30,6 @@ class ReceiptsController extends Controller
         ->whereDate('daily_records.arrival_datetime', '=', now()->toDateString())
         ->where('active', false)
         ->pluck('id');
-
-        
 
         $sellersRest = DB::table('daily_records')
         ->select(DB::raw('DATE_FORMAT(daily_records.arrival_datetime, "%d/%m/%Y %h:%i %p") as arrival_datetime'), DB::raw('DATE_FORMAT(daily_records.departure_datetime, "%d/%m/%Y %h:%i %p") as departure_datetime'), 'seller_name', 'seller_image', 'daily_records.id')
